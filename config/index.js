540f306d6d66e8481e169eca2c5825a7c77fcb6f
Chibuike Ewenike <sopewenike@yahoo.com>
2022-09-28 18:11:41 +0100

    testing env logic

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV === "development";
 //get the environment variable to use in prefixing fetch url
 export const server = dev
   ? "http://localhost:3000"
-  : "https://musknet.vercel.app/";
+  : "https://musknet.vercel.app";
