ba98070bef95e5eb57639ef3391bcb4e134bc458
Chibuike Ewenike <sopewenike@yahoo.com>
2022-09-28 18:03:54 +0100

    path bug checking

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV === "development";
 //get the environment variable to use in prefixing fetch url
 export const server = dev
   ? "http://localhost:3000"
-  : "https://musknet-chibuikee.vercel.app/";
+  : "https://musknet.vercel.app/";
---
+++ pages/texting.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function texting() {
+  return <div>texting</div>;
+}
+
+export default texting;
