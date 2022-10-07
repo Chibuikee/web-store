c253026b1dea990232df84f48f9b369ab86d3864
Chibuike Ewenike <sopewenike@yahoo.com>
2022-10-07 22:00:46 +0100

    fixed Blog bug

--- components/Navbar.js
+++ components/Navbar.js
@@ -135,7 +135,7 @@ function Navbar() {
               </div>
             </li>
             <li>
-              <Link href="/Blog">
+              <Link href="Blog">
                 <a>BLOG</a>
               </Link>
               <svg
